a0e2eec8b4e9d95f919892abe54df898235ec1de
Harry Slatyer <harry.slatyer@gmail.com>
2017-05-09 23:15:47 +1000

    Don't store unnecessary things

--- host/storage.py
+++ host/storage.py
@@ -1,17 +1,15 @@
 from google.appengine.ext import ndb
 
 class ImageItem(ndb.Model):
-    # TODO: Maybe we should just store the binary blob..
-    bdata = ndb.StringProperty(indexed=False)
-    ext = ndb.StringProperty()
+    data = ndb.BlobProperty(indexed=False)
     date = ndb.DateTimeProperty(auto_now_add=True)
 
-def put(bdata, ext):
-    item = ImageItem(bdata=bdata, ext=ext, cw_data=cw_data)
+def put(data):
+    item = ImageItem(data=data)
     return item.put().urlsafe()
 
 def get(key):
     item = ndb.Key(urlsafe=key).get()
     if not item:
         return None
-    return item.bdata, item.ext, item.date
+    return item.data, item.date
