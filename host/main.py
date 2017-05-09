89a98bc7b2e5f0c45ab4bc56c9012bc1a77f35d1
Harry Slatyer <harry.slatyer@gmail.com>
2017-05-09 23:16:46 +1000

    Add functionality for uploading/displaying images

--- host/main.py
+++ host/main.py
@@ -1,5 +1,6 @@
-from flask import Flask, request, render_template, url_for, make_response
+from flask import Flask, abort, request, render_template, url_for, make_response
 import base64
+import imghdr
 
 import storage
 
@@ -11,10 +12,38 @@ def welcome():
 
 @app.route('/img/<path:path>', methods=['GET'])
 def img(path):
-    # TODO: Get and display the image
-    return "Hello"
+    components = path.split(".")
+    key = components[0]
+    ext = ""
+    if len(components) >= 2:
+        ext = components[-1]
+
+    item = storage.get(key)
+    if not item:
+        return "not found", 404
+    data, _ = item
+
+    response = make_response(data)
+    response.headers['Content-Type'] = 'image/' + ext
+    return response
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    # TODO: Store the data and return a link
-    return url_for("img", path="foo")
+    data = get_data(request)
+    if not data:
+        return "missing data", 400
+    ext = get_ext(data)
+    if not ext:
+        return "could not determine type", 400
+    key = storage.put(data)
+    return url_for("img", path=key + "." + ext, _external=True)
+
+def get_data(request):
+    data = None
+    if 'image' in request.files:
+        file = request.files['image']
+        data = file.read();
+    return data
+
+def get_ext(data):
+    return imghdr.what(None, h=data)
