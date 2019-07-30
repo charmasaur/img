dfbc8046b2a358d1d664d2e564bea7eefbbb8d8a
Harry Slatyer <harry.slatyer@gmail.com>
2019-07-30 16:26:36 +1000

    Add jcrop for cropping images
    Should probably include it locally rather than loading it.

--- chrome/snip_image.js
+++ chrome/snip_image.js
@@ -1,3 +1,8 @@
 function setImageUrl(imageUrl) {
-  document.getElementById('image').src = imageUrl;
+    document.getElementById('image').src = imageUrl;
+
+    const jcrop = Jcrop.attach('image');
+    jcrop.listen('dblclick', function() {
+        console.log("Ready to send")
+    })
 }
