6a8de6bebcf35cdc03bb5619725eb52c8dd2b11f
Harry Slatyer <harry.slatyer@gmail.com>
2021-01-23 13:49:31 +1100

    Mock out storage

--- host/app/storage.py
+++ host/app/storage.py
@@ -1,21 +1,7 @@
-from google.appengine.ext import ndb
-from google.net.proto.ProtocolBuffer import ProtocolBufferDecodeError
-
-class ImageItem(ndb.Model):
-    data = ndb.BlobProperty(indexed=False)
-    date = ndb.DateTimeProperty(auto_now_add=True)
-
 def put(data):
-    item = ImageItem(data=data)
-    return item.put().urlsafe()
+    # Put data, return a key.
+    return "dummy"
 
 def get(key):
-    try:
-        item = ndb.Key(urlsafe=key).get()
-    except TypeError:
-        return None
-    except ProtocolBufferDecodeError:
-        return None
-    if not item:
-        return None
-    return item.data, item.date
+    # Get data and date associated with a key.
+    return "dummy", "dummy"
